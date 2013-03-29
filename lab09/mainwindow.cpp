34ab088f202976d40235b4e331e6bddbe69d800b
Haley Lenner <lenner@usc.edu>
2013-03-28 20:55:26 -0700

    Lab09 complete

--- lab09/mainwindow.cpp
+++ lab09/mainwindow.cpp
@@ -1,24 +1,93 @@
 #include "mainwindow.h"
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
 
 void MainWindow::handleTimer() {
-    item->move( WINDOW_MAX_X, WINDOW_MAX_Y );
+    //item->move( WINDOW_MAX_X, WINDOW_MAX_Y );
+    counter++;
+    int w = rand() % 20 +5;
+    int h = rand() % 20+5;
+    double y= rand()% 100;
+    double x = rand()% 100;
+    double vx = rand() % 5 + 1;
+    double vy = rand() % 5 + 1;
+    QBrush redBrush(Qt::red);
+    QBrush blueBrush(Qt::blue);
+    QBrush greenBrush(Qt::green);
+    QBrush yellowBrush(Qt::yellow);
+    for (int i=0; i<rectangles.size(); i++){
+    	rectangles[i]->move(WINDOW_MAX_X, WINDOW_MAX_Y);
+    	}
+   // std::cout << counter << std::endl;
+    if (counter%50==0){
+    	BouncingRectangle *p = new BouncingRectangle( x, y, w, h, vx, vy );
+    	rectangles.push_back(p);
+    	
+    int i = rand() % 4;
+    if (i==0){
+    p->setBrush( redBrush );
+    }
+    if (i==1){
+    p->setBrush( blueBrush );
+    }
+    if (i==2){
+    p->setBrush( greenBrush );
+    }
+    if (i==3){
+    p->setBrush( yellowBrush );
+     }	
+     scene->addItem( p); 
+    }
+    
+    
 }
+void MainWindow::handleButton(){
 
+    if (timer->isActive()==true){
+    	timer->stop();
+    	button->setText("Start");
+    }
+    else {
+	timer->start();
+    	button->setText("Stop");
+     }
+}
 MainWindow::MainWindow()  {
+    counter=0;
     //We need a scene and a view to do graphics in QT
     scene = new QGraphicsScene();
     view = new QGraphicsView( scene );
-
+    
+    
     //To fill a rectangle use a QBrush. To draw the border of a shape, use a QPen
     QBrush redBrush(Qt::red);
+    QBrush blueBrush(Qt::blue);
+    QBrush greenBrush(Qt::green);
+    QBrush yellowBrush(Qt::yellow);
 
     //First 2 arguments are the x, y, of the upper left of the rectangle.
     //The second 2 arguments are the width and height
     //The last 2 arguments are the velocity in the x, and y, directions
-    item = new BouncingRectangle( 11.0, 74.0, 20.0, 20.0, 2, 3 );
+   
+   
+    item = new BouncingRectangle( 11, 73, 20, 20, 2, 3 );
+    	
+    int i = rand() % 4;
+    if (i==0){
     item->setBrush( redBrush );
+    }
+    if (i==1){
+    item->setBrush( blueBrush );
+    }
+    if (i==2){
+    item->setBrush( greenBrush );
+    }
+    if (i==3){
+    item->setBrush( yellowBrush );
+    }
     scene->addItem( item );
-
+    
     //This sets the size of the window and gives it a title.
     view->setFixedSize( WINDOW_MAX_X*2, WINDOW_MAX_Y*2 );
     view->setWindowTitle( "Graphical 8-Tile Puzzle");
@@ -28,20 +97,22 @@ MainWindow::MainWindow()  {
     //of our own - called handleTimer - which is in this same MainWindow class
     timer = new QTimer(this);
     timer->setInterval(5);
+
     connect(timer, SIGNAL(timeout()), this, SLOT(handleTimer()));
-
-
+    button = new QPushButton(view);
+    button->setText("Start");
+    button->setGeometry(225, 450, 60, 40);
+    
+    connect(button, SIGNAL(clicked()), this, SLOT(handleButton()));
+    timer->start();
+    //button->show();
 }
-
 void MainWindow::show() {
     //This is only called once by main. Timers don't start working until
     //we call start
-    timer->start();
-
     //This is how we get our view displayed.
     view->show();
 }
-
 MainWindow::~MainWindow()
 {
     timer->stop();
@@ -50,4 +121,3 @@ MainWindow::~MainWindow()
     delete scene;
     delete view;
 }
-
--- lab09/mainwindow.h
+++ lab09/mainwindow.h
@@ -9,30 +9,33 @@
 #include <QTimeLine>
 #include <QGraphicsItemAnimation>
 #include <QPushButton>
+#include <vector>
 
 #include "bouncingrectangle.h"
 
 #define WINDOW_MAX_X 250
 #define WINDOW_MAX_Y 250
 
+using namespace std;
 class MainWindow : public QWidget {
     Q_OBJECT
     
 public:
     explicit MainWindow();
     ~MainWindow();
-
     void show();
-    
+    int counter;
+    vector <BouncingRectangle*> rectangles;
 private:
     QGraphicsScene *scene;
     QGraphicsView *view;
     QTimer *timer;
     BouncingRectangle *item;
+    QPushButton *button;
 
 public slots:
     void handleTimer();
-
+    void handleButton();
 };
 
 #endif // MAINWINDOW_H
